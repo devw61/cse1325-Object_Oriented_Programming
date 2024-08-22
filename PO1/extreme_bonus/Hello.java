6fd6bad6c6b52f99ca075382c3f91ad348a9e52b
devw45 <devin.wingfield45@gmail.com>
2024-08-22 11:36:56 -0500

    correct version of automatically geting user's name

--- PO1/extreme_bonus/Hello.java
+++ PO1/extreme_bonus/Hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String name = System.geten("USERNAME");
+		String name = System.getProperty("user.name");				
                 System.out.println("Hello, " + name);
         }		
 }
