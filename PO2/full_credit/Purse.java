ff915629d83e34fa121b0dbf6dd0462d45192fd8
devw45 <devin.wingfield45@gmail.com>
2024-08-30 10:20:07 -0500

    structure complete

---
+++ PO2/full_credit/Purse.java
@@ -0,0 +1,25 @@
+public enum Denomination { PENNY, NICKEL, DIME, QUARTER}
+
+public class Coin {
+	private Denomination denomination;
+	private int year;
+
+	public Coin(denomination: Denomination, year: int) {
+		this.denomination = denomination;
+		this.year = year;
+	} 
+
+	public double getValue() {
+		
+	}
+
+	public int getYear() {
+
+	}
+}
+
+public class purse () {
+	public static void main(String [] args1) {
+		
+	}
+}
