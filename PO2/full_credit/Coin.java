feae0af6b5f2036f249f48b251c6b881a2368d5b
devw45 <devin.wingfield45@gmail.com>
2024-08-30 11:55:13 -0500

    fixed structural errors, put each method in it's own file

---
+++ PO2/full_credit/Coin.java
@@ -0,0 +1,35 @@
+public class Coin {
+        private Denomination denomination;
+        private int year;
+
+        public Coin(Denomination denomination, int year) {
+                this.denomination = denomination;
+                this.year = year;
+        }
+
+        public double getValue() {
+                switch(denomination) {
+                        case PENNY:
+                                return .01;
+                        case NICKEL:
+                                return .05;
+                        case DIME:
+                                return .10;
+                        case QUARTER:
+                                return .25;
+                        default:
+                                System.out.print("invalid coin");
+				return 0.0;
+                }
+        }
+
+        public int getYear() {
+                return year;
+        }
+	
+	@Override
+	public String toString() {
+		return "testing";
+	}
+}
+
