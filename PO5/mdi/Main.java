f3a794dacd4a6054e6c5c676d38365b10bb12e2a
devw45 <devin.wingfield45@gmail.com>
2024-09-29 23:20:37 -0500

    added rest of the methods + main loop

--- PO5/mdi/Main.java
+++ PO5/mdi/Main.java
@@ -9,13 +9,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		Main mainInstance = new Main(new Moes(), new Menu(), "", true);
+		mainInstance.mdi();
 	}
 
 
 	private Moes moes = new Moes();
 	private Menu menu = new Menu();
-	String output;
-	boolean running;
+	private String output;
+	private boolean running;
 
 	private void addStudent(){
 		Scanner in = new Scanner(System.in);
@@ -85,7 +87,7 @@ public class Main {
 		System.out.print("Enter Student's index: ");
 		int student_index;
                 while (!in.hasNextInt()) {
-                    System.out.println("Invalid input. Please enter an integer for ID: ");
+                    System.out.print("Invalid input. Please enter an integer for ID: ");
                     in.nextLine();
                 }
                 student_index = in.nextInt();
@@ -95,17 +97,18 @@ public class Main {
 		System.out.print("Enter Media index: ");
 		int media_index;
 		while (!in.hasNextInt()) {
-                    System.out.println("Invalid input. Please enter an integer for ID: ");
+                    System.out.print("Invalid input. Please enter an integer for ID: ");
                     in.nextLine();
                 }
 		media_index = in.nextInt();
 
-		moes.playMedia(student_index, media_index);
+		System.out.println(moes.playMedia(student_index, media_index));
 	}
 
 	private void listAvailablePoints() {
 		Scanner in = new Scanner(System.in);
-	
+		
+		System.out.print("Enter Student's index: ");
 		int student_index;
                 while (!in.hasNextInt()) {
                     System.out.println("Invalid input. Please enter an integer for ID: ");
@@ -127,11 +130,51 @@ public class Main {
 		int bought_points = -1;
 		while (bought_points <= 0){
 			System.out.print("Enter the amount of points you want to buy: ");
-			bought_points = in.nextInt() > 0 ? in.nextInt() : -1;
-			System.out.println("Invalid Amount!");
+			bought_points = in.nextInt();
+			if (bought_points < 0) {System.out.println("Invalid Amount!");} 
 		}
 		in.nextLine();
 
-		moes.buyPoints(student_index, bought_points);
+		System.out.println(moes.buyPoints(student_index, bought_points));
+	}
+
+	public Main(Moes moes, Menu menu, String output, boolean running){
+		this.moes = moes;
+	    this.menu = menu;
+		this.output = output;
+		this.running = running;
+		
+		menu.addMenuItem(new MenuItem("Add a Student",     () -> addStudent()));
+		menu.addMenuItem(new MenuItem("List Students",       () -> listStudents()));
+		menu.addMenuItem(new MenuItem("Add Media",      () -> addMedia()));
+		menu.addMenuItem(new MenuItem("Play Media",         () -> playMedia()));
+		menu.addMenuItem(new MenuItem("List Media",       () -> listMedia()));
+		menu.addMenuItem(new MenuItem("List Available Points",     () -> listAvailablePoints()));
+		menu.addMenuItem(new MenuItem("Buy Points",     () -> buyPoints()));
+		menu.addMenuItem(new MenuItem("Exit",               () -> endApp()));
+	
+	}
+
+	private void mdi(){
+		Scanner in = new Scanner(System.in);
+		System.out.print("\033[H\033[2J"); // clear the console
+		System.out.println("Welcome to Moes!");
+		
+		while (running) {
+			System.out.println("======================");
+			System.out.print(menu.toString());
+
+			System.out.print("Choice: ");
+			int choice = in.nextInt();
+			in.nextLine();
+
+			boolean valid = choice >= 0;
+			System.out.println("======================");
+			if (valid) {menu.run(choice);} else {System.out.println("Invalid choice!");}
+		}
+	}
+
+	private void endApp(){
+		this.running = false;
 	}
 }
