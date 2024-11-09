0c2419e871223fc987f3607760b2facfbff0cd2d
devw45 <devin.wingfield45@gmail.com>
2024-11-09 17:49:15 -0600

    finished operator<<

--- P10/full_credit/purse.h
+++ P10/full_credit/purse.h
@@ -6,7 +6,7 @@
 class Purse {
 	public:
 		Purse(int pounds = 0, int shillings = 0, int pence = 0);
-		friend std::ostream operator<<(std::ostream ost, const Purse& purse);
+		friend std::ostream& operator<<(std::ostream& ost, const Purse& purse);
 
 	private:
 		int _pounds;
