bc9874dcd546fbfebb4c513d79ed031e3a46409a
devw45 <devin.wingfield45@gmail.com>
2024-11-11 09:47:28 -0600

    added operator>> to purse.h - bonus

---
+++ P10/bonus/purse.cpp
@@ -0,0 +1,75 @@
+#include "purse.h"
+
+Purse::Purse(int pounds, int shillings, int pence)
+	: _pounds{pounds}, _shillings{shillings}, _pence{pence} {
+		rationalize();
+}
+
+std::ostream& operator<<(std::ostream& os, const Purse& purse){
+	os << "£" << purse._pounds << " " << purse._shillings << "s" << purse._pence << "d";
+	return os;
+}
+
+Purse& Purse::operator++(){
+	++_pence;
+	rationalize();
+	return *this;
+}
+
+Purse Purse::operator++(int){
+	Purse old_purse{*this};
+	++_pence;
+	rationalize();
+	return old_purse;
+}
+
+Purse Purse::operator+(const Purse& other) {
+    Purse result{_pounds + other._pounds, _shillings + other._shillings, _pence + other._pence};
+    result.rationalize();
+    return result;
+}
+
+Purse Purse::operator-(const Purse& other) {
+    Purse result{_pounds - other._pounds, _shillings - other._shillings, _pence - other._pence};
+    result.rationalize();
+    return result;
+}
+
+Purse& Purse::operator+=(const Purse& other) {
+	_pounds += other._pounds;
+	_shillings += other._shillings;
+	_pence += other._pence;
+	rationalize();
+	return *this;
+}
+
+Purse& Purse::operator-=(const Purse& other) {
+	_pounds -= other._pounds;
+	_shillings -= other._shillings;
+	_pence -= other._pence;
+	rationalize();
+	return *this;
+}
+
+void Purse::rationalize() {
+	if (_pence < 0) {
+		int borrow_shillings = (-_pence + 11) / 12;
+		_shillings -= borrow_shillings;
+		_pence += borrow_shillings * 12;
+	}
+	if (_pence >= 12) {
+		_shillings += _pence / 12;
+		_pence %= 12;
+	}
+
+	if (_shillings < 0) {
+		int borrow_pounds = (-_shillings + 19) / 20;
+		_pounds -= borrow_pounds;
+		_shillings += borrow_pounds * 20;
+	}
+	if (_shillings >= 20) {
+		_pounds += _shillings / 20;
+		_shillings %= 20;
+	}
+}
+
---
+++ P10/bonus/purse.h
@@ -0,0 +1,26 @@
+#ifndef __PURSE_H
+#define __PURSE_H
+
+#include <iostream>
+
+class Purse {
+	public:
+		Purse(int pounds = 0, int shillings = 0, int pence = 0);
+		friend std::ostream& operator<<(std::ostream& ost, const Purse& purse);
+		friend std::istream &operator>>(std::istream &ost, const Purse &purse);
+		auto operator<=>(const Purse&) const = default;
+		Purse& operator++();
+		Purse operator++(int);
+		Purse operator+(const Purse&);
+		Purse operator-(const Purse&);
+		Purse& operator+=(const Purse&);
+		Purse& operator-=(const Purse&);
+		void rationalize();
+
+	private:
+		int _pounds;
+		int _shillings;
+		int _pence;
+};
+
+#endif
