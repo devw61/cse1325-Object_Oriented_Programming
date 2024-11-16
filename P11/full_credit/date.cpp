c031630fed7c6f3b943703a35f607044cc7a3fcb
devw45 <devin.wingfield45@gmail.com>
2024-11-16 17:46:27 -0600

    P11: can load temps.csv - full_credit

--- P11/full_credit/date.cpp
+++ P11/full_credit/date.cpp
@@ -3,9 +3,9 @@
 
 Date::Date(int year, int month, int day)
 	: _year(year), _month(month), _day(day) {
-		if (_year < 1970 || _year > 2024) throw std::out_of_range{"Error - year must be between 1970 and 2024"};
-		if (_month < 1 || _month > 12) throw std::out_of_range{"Error - month must be between 1 and 12"};
-		if (_day < 1 || _day > 31) throw std::out_of_range{"Error - day must be between 1 and 31"};
+		if (_year < 1970 || _year > 2024) throw std::out_of_range{"Error - year must be between 1970 and 2024: " + std::to_string(_year)};
+		if (_month < 1 || _month > 12) throw std::out_of_range{"Error - month must be between 1 and 12: " + std::to_string(_month)};
+		if (_day < 1 || _day > 31) throw std::out_of_range{"Error - day must be between 1 and 31: " + std::to_string(_day)};
 }
 
 std::ostream& operator<<(std::ostream& ost, const Date& date) {
--- P11/full_credit/list_temps.cpp
+++ P11/full_credit/list_temps.cpp
@@ -9,16 +9,34 @@ typedef std::map<Date, Temp> temps;
 int main(int argc, char* argv[]){
     	if (argc != 2) {
 		std::cerr << "Usage: <program name> <data file>" << std::endl;
+        return -1;
     	}
 	
 	std::ifstream ifs {std::string{argv[1]}};
 	if (!ifs) {
         	throw std::runtime_error{"Can't open input file " + std::string{argv[1]}};
-	        return -1;
+	        return -2;
     	}
 
 	while (ifs){
-		for (int i = 0; i < 4; i++) ifs.ignore();
-	}
-	
+		std::string val;
+        int year, month, day;
+
+		for (int i = 0; i < 7; i++){
+            if (i == 4){
+                std::getline(ifs, val, ',');
+                month = std::stoi(val);
+            } else if (i == 5){
+                std::getline(ifs, val, ',');
+                day = std::stoi(val);
+            } else if (i == 6){
+                std::getline(ifs, val, ',');
+                year = std::stoi(val);
+            } else {
+                std::getline(ifs, val, ',');
+            } 
+        }
+        Date date{year, month, day};
+    }
+        
 }
