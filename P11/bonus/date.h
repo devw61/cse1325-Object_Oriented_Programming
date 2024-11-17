3ea9a4a844f26f2d863d4b842be365e80ae3a41d
devw45 <devin.wingfield45@gmail.com>
2024-11-16 18:24:19 -0600

    created bonus

---
+++ P11/bonus/date.h
@@ -0,0 +1,17 @@
+#ifndef __DATE_H
+#define __DATE_H
+
+#include <iostream>
+
+class Date {
+	public:
+		Date(int year, int month, int day);
+		friend std::ostream& operator<<(std::ostream& ost, const Date& date);
+		auto operator<=>(const Date&) const = default;
+	private:
+		int _year;
+		int _month;
+		int _day;
+};
+
+#endif
